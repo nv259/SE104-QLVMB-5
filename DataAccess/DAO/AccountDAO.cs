f9b5b6a4b75a2d373ee2b349b5205392faa26faf
nhatnv0378 <nguyenvietnhat.03@gmail.com>
2022-12-20 20:29:20 +0700

    Add DAO Account

---
+++ DataAccess/DAO/AccountDAO.cs
@@ -0,0 +1,32 @@
+﻿using DataAccess.DTO;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.DAO
+{
+    public class AccountDAO
+    {
+        private Account acc;
+        private static AccountDAO instance;
+
+        public static AccountDAO Instance { get { if (instance == null) instance = new AccountDAO(); return instance; } }
+
+        public Account GetAccountByUserName(string name)
+        {
+            string query = "SELECT * FROM dbo.Account WHERE MaDangNhap = @name ";
+            DataTable dt = new DataTable();
+            dt = DataProvider.Instance.ExecuteQuery(query, new object[] { name });
+            
+            foreach (DataRow dr in dt.Rows)
+            {
+                return new Account(dr);
+            }
+
+            return null;
+        }
+    }
+}
