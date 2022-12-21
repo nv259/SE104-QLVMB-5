c7db564b249867d60ccbcbeb076bad4759c2c94b
nhatnv0378 <nguyenvietnhat.03@gmail.com>
2022-12-22 01:49:25 +0700

    add BillForm (BM3)

---
+++ Service/Bill.Designer.cs
@@ -0,0 +1,371 @@
+﻿namespace Service
+{
+    partial class Bill
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.cb_cmbBox = new System.Windows.Forms.ComboBox();
+            this.Cost_txtBox = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.email_txtBox = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.phone_txtBox = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.ID_txtBox = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.name_txtBox = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.ticket_cmbBox = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.ngDatVeBox = new System.Windows.Forms.DateTimePicker();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox14 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.cb_cmbBox);
+            this.panel1.Controls.Add(this.Cost_txtBox);
+            this.panel1.Controls.Add(this.textBox12);
+            this.panel1.Controls.Add(this.email_txtBox);
+            this.panel1.Controls.Add(this.textBox11);
+            this.panel1.Controls.Add(this.phone_txtBox);
+            this.panel1.Controls.Add(this.textBox9);
+            this.panel1.Controls.Add(this.ID_txtBox);
+            this.panel1.Controls.Add(this.textBox5);
+            this.panel1.Controls.Add(this.name_txtBox);
+            this.panel1.Controls.Add(this.textBox3);
+            this.panel1.Controls.Add(this.ticket_cmbBox);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.ngDatVeBox);
+            this.panel1.Controls.Add(this.textBox8);
+            this.panel1.Location = new System.Drawing.Point(0, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(427, 281);
+            this.panel1.TabIndex = 0;
+            // 
+            // cb_cmbBox
+            // 
+            this.cb_cmbBox.FormattingEnabled = true;
+            this.cb_cmbBox.Location = new System.Drawing.Point(203, 25);
+            this.cb_cmbBox.Name = "cb_cmbBox";
+            this.cb_cmbBox.Size = new System.Drawing.Size(104, 28);
+            this.cb_cmbBox.TabIndex = 35;
+            // 
+            // Cost_txtBox
+            // 
+            this.Cost_txtBox.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(248)))), ((int)(((byte)(242)))), ((int)(((byte)(235)))));
+            this.Cost_txtBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.Cost_txtBox.Location = new System.Drawing.Point(261, 242);
+            this.Cost_txtBox.Name = "Cost_txtBox";
+            this.Cost_txtBox.ReadOnly = true;
+            this.Cost_txtBox.Size = new System.Drawing.Size(161, 27);
+            this.Cost_txtBox.TabIndex = 34;
+            this.Cost_txtBox.TabStop = false;
+            // 
+            // textBox12
+            // 
+            this.textBox12.BackColor = System.Drawing.Color.White;
+            this.textBox12.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox12.Font = new System.Drawing.Font("Segoe UI", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Underline))), System.Drawing.GraphicsUnit.Point);
+            this.textBox12.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(117)))), ((int)(((byte)(81)))));
+            this.textBox12.Location = new System.Drawing.Point(196, 244);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.ReadOnly = true;
+            this.textBox12.Size = new System.Drawing.Size(59, 20);
+            this.textBox12.TabIndex = 33;
+            this.textBox12.TabStop = false;
+            this.textBox12.Text = "Giá tiền:";
+            // 
+            // email_txtBox
+            // 
+            this.email_txtBox.BackColor = System.Drawing.Color.White;
+            this.email_txtBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.email_txtBox.Location = new System.Drawing.Point(203, 181);
+            this.email_txtBox.Name = "email_txtBox";
+            this.email_txtBox.Size = new System.Drawing.Size(219, 27);
+            this.email_txtBox.TabIndex = 32;
+            this.email_txtBox.TabStop = false;
+            // 
+            // textBox11
+            // 
+            this.textBox11.BackColor = System.Drawing.Color.White;
+            this.textBox11.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox11.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(117)))), ((int)(((byte)(81)))));
+            this.textBox11.Location = new System.Drawing.Point(203, 155);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.ReadOnly = true;
+            this.textBox11.Size = new System.Drawing.Size(104, 20);
+            this.textBox11.TabIndex = 31;
+            this.textBox11.TabStop = false;
+            this.textBox11.Text = "Email:";
+            // 
+            // phone_txtBox
+            // 
+            this.phone_txtBox.BackColor = System.Drawing.Color.White;
+            this.phone_txtBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.phone_txtBox.Location = new System.Drawing.Point(3, 181);
+            this.phone_txtBox.Name = "phone_txtBox";
+            this.phone_txtBox.Size = new System.Drawing.Size(170, 27);
+            this.phone_txtBox.TabIndex = 30;
+            this.phone_txtBox.TabStop = false;
+            // 
+            // textBox9
+            // 
+            this.textBox9.BackColor = System.Drawing.Color.White;
+            this.textBox9.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox9.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox9.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(117)))), ((int)(((byte)(81)))));
+            this.textBox9.Location = new System.Drawing.Point(3, 155);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(92, 20);
+            this.textBox9.TabIndex = 29;
+            this.textBox9.TabStop = false;
+            this.textBox9.Text = "Số điện thoại:";
+            // 
+            // ID_txtBox
+            // 
+            this.ID_txtBox.BackColor = System.Drawing.Color.White;
+            this.ID_txtBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.ID_txtBox.Location = new System.Drawing.Point(203, 99);
+            this.ID_txtBox.Name = "ID_txtBox";
+            this.ID_txtBox.Size = new System.Drawing.Size(219, 27);
+            this.ID_txtBox.TabIndex = 28;
+            this.ID_txtBox.TabStop = false;
+            // 
+            // textBox5
+            // 
+            this.textBox5.BackColor = System.Drawing.Color.White;
+            this.textBox5.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox5.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox5.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(117)))), ((int)(((byte)(81)))));
+            this.textBox5.Location = new System.Drawing.Point(203, 73);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(211, 20);
+            this.textBox5.TabIndex = 27;
+            this.textBox5.TabStop = false;
+            this.textBox5.Text = "Chứng minh thư/CCCD:";
+            // 
+            // name_txtBox
+            // 
+            this.name_txtBox.BackColor = System.Drawing.Color.White;
+            this.name_txtBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.name_txtBox.Location = new System.Drawing.Point(3, 99);
+            this.name_txtBox.Name = "name_txtBox";
+            this.name_txtBox.Size = new System.Drawing.Size(170, 27);
+            this.name_txtBox.TabIndex = 26;
+            this.name_txtBox.TabStop = false;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.Color.White;
+            this.textBox3.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(117)))), ((int)(((byte)(81)))));
+            this.textBox3.Location = new System.Drawing.Point(3, 73);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(92, 20);
+            this.textBox3.TabIndex = 25;
+            this.textBox3.TabStop = false;
+            this.textBox3.Text = "Hành khách:";
+            // 
+            // ticket_cmbBox
+            // 
+            this.ticket_cmbBox.FormattingEnabled = true;
+            this.ticket_cmbBox.Location = new System.Drawing.Point(318, 25);
+            this.ticket_cmbBox.Name = "ticket_cmbBox";
+            this.ticket_cmbBox.Size = new System.Drawing.Size(104, 28);
+            this.ticket_cmbBox.TabIndex = 24;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.White;
+            this.textBox2.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(117)))), ((int)(((byte)(81)))));
+            this.textBox2.Location = new System.Drawing.Point(318, 3);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(92, 20);
+            this.textBox2.TabIndex = 23;
+            this.textBox2.TabStop = false;
+            this.textBox2.Text = "Hạng vé:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.White;
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(117)))), ((int)(((byte)(81)))));
+            this.textBox1.Location = new System.Drawing.Point(203, 3);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(92, 20);
+            this.textBox1.TabIndex = 21;
+            this.textBox1.TabStop = false;
+            this.textBox1.Text = "Chuyến bay:";
+            // 
+            // ngDatVeBox
+            // 
+            this.ngDatVeBox.CalendarMonthBackground = System.Drawing.SystemColors.Control;
+            this.ngDatVeBox.CustomFormat = "MM/dd/yyyy";
+            this.ngDatVeBox.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.ngDatVeBox.Location = new System.Drawing.Point(3, 26);
+            this.ngDatVeBox.MinDate = new System.DateTime(2022, 12, 21, 11, 25, 7, 0);
+            this.ngDatVeBox.Name = "ngDatVeBox";
+            this.ngDatVeBox.Size = new System.Drawing.Size(170, 27);
+            this.ngDatVeBox.TabIndex = 20;
+            this.ngDatVeBox.Value = new System.DateTime(2022, 12, 21, 12, 43, 4, 0);
+            // 
+            // textBox8
+            // 
+            this.textBox8.BackColor = System.Drawing.Color.White;
+            this.textBox8.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.textBox8.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox8.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(117)))), ((int)(((byte)(81)))));
+            this.textBox8.Location = new System.Drawing.Point(3, 3);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(92, 20);
+            this.textBox8.TabIndex = 12;
+            this.textBox8.TabStop = false;
+            this.textBox8.Text = "Ngày đặt vé:";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(117)))), ((int)(((byte)(81)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(151, 341);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(131, 47);
+            this.button1.TabIndex = 35;
+            this.button1.Text = "Đặt Vé";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // textBox14
+            // 
+            this.textBox14.BackColor = System.Drawing.Color.White;
+            this.textBox14.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox14.Font = new System.Drawing.Font("Segoe UI", 19F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.textBox14.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(164)))), ((int)(((byte)(117)))), ((int)(((byte)(81)))));
+            this.textBox14.Location = new System.Drawing.Point(0, -4);
+            this.textBox14.Name = "textBox14";
+            this.textBox14.ReadOnly = true;
+            this.textBox14.Size = new System.Drawing.Size(427, 50);
+            this.textBox14.TabIndex = 36;
+            this.textBox14.TabStop = false;
+            this.textBox14.Text = "Vé chuyến bay";
+            this.textBox14.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.IndianRed;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button2.ForeColor = System.Drawing.Color.White;
+            this.button2.Location = new System.Drawing.Point(288, 359);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(95, 29);
+            this.button2.TabIndex = 37;
+            this.button2.Text = "Hủy";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.IndianRed;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button3.ForeColor = System.Drawing.Color.White;
+            this.button3.Location = new System.Drawing.Point(50, 359);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(95, 29);
+            this.button3.TabIndex = 38;
+            this.button3.Text = "Đóng";
+            this.button3.UseVisualStyleBackColor = false;
+            // 
+            // Bill
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(427, 419);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox14);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Bill";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "BillForm";
+            this.Load += new System.EventHandler(this.Bill_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Panel panel1;
+        private TextBox textBox8;
+        internal DateTimePicker ngDatVeBox;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private ComboBox ticket_cmbBox;
+        private TextBox name_txtBox;
+        private TextBox ID_txtBox;
+        private TextBox textBox5;
+        private TextBox email_txtBox;
+        private TextBox textBox11;
+        private TextBox phone_txtBox;
+        private TextBox textBox9;
+        private TextBox textBox12;
+        private TextBox Cost_txtBox;
+        private Button button1;
+        private ComboBox cb_cmbBox;
+        private TextBox textBox14;
+        private Button button2;
+        private Button button3;
+    }
+}
---
+++ Service/Bill.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Service
+{
+    public partial class Bill : Form
+    {
+        public Bill()
+        {
+            InitializeComponent();
+        }
+
+        private void Bill_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
