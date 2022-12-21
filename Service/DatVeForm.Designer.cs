5d58d7eb2e077b84065e8a5d83682ada79c8f00b
hhoangcpascal <havanhoang2911@gmail.com>
2022-12-21 12:37:41 +0700

    Modify DatVeForm

--- Service/DatVeForm.Designer.cs
+++ Service/DatVeForm.Designer.cs
@@ -28,102 +28,116 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.flightListLstBox = new System.Windows.Forms.ListBox();
             this.fromComboBox = new System.Windows.Forms.ComboBox();
             this.toComboBox = new System.Windows.Forms.ComboBox();
             this.ngayBayDtp = new System.Windows.Forms.DateTimePicker();
-            this.flightInfoLstBox = new System.Windows.Forms.ListBox();
             this.infoTicketLstBox = new System.Windows.Forms.ListBox();
             this.ticketPickComboBox = new System.Windows.Forms.ComboBox();
             this.datVeBtn = new System.Windows.Forms.Button();
+            this.airportBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.Find_Btn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.airportBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
             // flightListLstBox
             // 
             this.flightListLstBox.FormattingEnabled = true;
             this.flightListLstBox.ItemHeight = 20;
-            this.flightListLstBox.Location = new System.Drawing.Point(45, 101);
+            this.flightListLstBox.Location = new System.Drawing.Point(45, 141);
             this.flightListLstBox.Name = "flightListLstBox";
-            this.flightListLstBox.Size = new System.Drawing.Size(339, 424);
+            this.flightListLstBox.Size = new System.Drawing.Size(459, 384);
             this.flightListLstBox.TabIndex = 0;
+            this.flightListLstBox.SelectedIndexChanged += new System.EventHandler(this.flightListLstBox_SelectedIndexChanged);
             // 
             // fromComboBox
             // 
             this.fromComboBox.FormattingEnabled = true;
             this.fromComboBox.Location = new System.Drawing.Point(45, 32);
             this.fromComboBox.Name = "fromComboBox";
-            this.fromComboBox.Size = new System.Drawing.Size(157, 28);
+            this.fromComboBox.Size = new System.Drawing.Size(212, 28);
             this.fromComboBox.TabIndex = 1;
-            this.fromComboBox.Text = "Bay từ";
+            this.fromComboBox.SelectedIndexChanged += new System.EventHandler(this.fromComboBox_SelectedIndexChanged);
             // 
             // toComboBox
             // 
             this.toComboBox.FormattingEnabled = true;
-            this.toComboBox.Location = new System.Drawing.Point(224, 32);
+            this.toComboBox.Location = new System.Drawing.Point(278, 32);
             this.toComboBox.Name = "toComboBox";
-            this.toComboBox.Size = new System.Drawing.Size(160, 28);
+            this.toComboBox.Size = new System.Drawing.Size(226, 28);
             this.toComboBox.TabIndex = 2;
             this.toComboBox.Text = "Bay đến";
+            this.toComboBox.SelectedIndexChanged += new System.EventHandler(this.toComboBox_SelectedIndexChanged);
             // 
             // ngayBayDtp
             // 
             this.ngayBayDtp.Location = new System.Drawing.Point(45, 66);
+            this.ngayBayDtp.MinDate = new System.DateTime(2022, 12, 21, 11, 25, 7, 0);
             this.ngayBayDtp.Name = "ngayBayDtp";
-            this.ngayBayDtp.Size = new System.Drawing.Size(339, 27);
+            this.ngayBayDtp.Size = new System.Drawing.Size(459, 27);
             this.ngayBayDtp.TabIndex = 3;
-            this.ngayBayDtp.Value = new System.DateTime(2022, 12, 20, 10, 39, 37, 0);
-            // 
-            // flightInfoLstBox
-            // 
-            this.flightInfoLstBox.FormattingEnabled = true;
-            this.flightInfoLstBox.ItemHeight = 20;
-            this.flightInfoLstBox.Location = new System.Drawing.Point(420, 34);
-            this.flightInfoLstBox.Name = "flightInfoLstBox";
-            this.flightInfoLstBox.Size = new System.Drawing.Size(309, 184);
-            this.flightInfoLstBox.TabIndex = 4;
+            this.ngayBayDtp.Value = new System.DateTime(2022, 12, 21, 11, 25, 7, 0);
+            this.ngayBayDtp.ValueChanged += new System.EventHandler(this.ngayBayDtp_ValueChanged);
             // 
             // infoTicketLstBox
             // 
             this.infoTicketLstBox.FormattingEnabled = true;
             this.infoTicketLstBox.ItemHeight = 20;
-            this.infoTicketLstBox.Location = new System.Drawing.Point(422, 241);
+            this.infoTicketLstBox.Location = new System.Drawing.Point(534, 248);
             this.infoTicketLstBox.Name = "infoTicketLstBox";
-            this.infoTicketLstBox.Size = new System.Drawing.Size(306, 104);
+            this.infoTicketLstBox.Size = new System.Drawing.Size(408, 104);
             this.infoTicketLstBox.TabIndex = 5;
             // 
             // ticketPickComboBox
             // 
             this.ticketPickComboBox.FormattingEnabled = true;
-            this.ticketPickComboBox.Location = new System.Drawing.Point(427, 358);
+            this.ticketPickComboBox.Location = new System.Drawing.Point(534, 358);
             this.ticketPickComboBox.Name = "ticketPickComboBox";
-            this.ticketPickComboBox.Size = new System.Drawing.Size(296, 28);
+            this.ticketPickComboBox.Size = new System.Drawing.Size(398, 28);
             this.ticketPickComboBox.TabIndex = 6;
             this.ticketPickComboBox.Text = "Chọn loại vé";
             // 
             // datVeBtn
             // 
-            this.datVeBtn.Location = new System.Drawing.Point(503, 434);
+            this.datVeBtn.Location = new System.Drawing.Point(679, 445);
             this.datVeBtn.Name = "datVeBtn";
             this.datVeBtn.Size = new System.Drawing.Size(158, 62);
             this.datVeBtn.TabIndex = 7;
             this.datVeBtn.Text = "Đặt vé";
             this.datVeBtn.UseVisualStyleBackColor = true;
             // 
+            // airportBindingSource
+            // 
+            this.airportBindingSource.DataSource = typeof(DataAccess.DTO.Airport);
+            // 
+            // Find_Btn
+            // 
+            this.Find_Btn.Location = new System.Drawing.Point(164, 106);
+            this.Find_Btn.Name = "Find_Btn";
+            this.Find_Btn.Size = new System.Drawing.Size(221, 29);
+            this.Find_Btn.TabIndex = 8;
+            this.Find_Btn.Text = "Tìm kiếm";
+            this.Find_Btn.UseVisualStyleBackColor = true;
+            this.Find_Btn.Click += new System.EventHandler(this.Find_Btn_Click);
+            // 
             // DatVeForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(750, 549);
+            this.ClientSize = new System.Drawing.Size(957, 549);
+            this.Controls.Add(this.Find_Btn);
             this.Controls.Add(this.datVeBtn);
             this.Controls.Add(this.ticketPickComboBox);
             this.Controls.Add(this.infoTicketLstBox);
-            this.Controls.Add(this.flightInfoLstBox);
             this.Controls.Add(this.ngayBayDtp);
             this.Controls.Add(this.toComboBox);
             this.Controls.Add(this.fromComboBox);
             this.Controls.Add(this.flightListLstBox);
             this.Name = "DatVeForm";
             this.Text = "Đặt vé";
+            this.Load += new System.EventHandler(this.DatVeForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.airportBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -134,9 +148,10 @@
         private ComboBox fromComboBox;
         private ComboBox toComboBox;
         private DateTimePicker ngayBayDtp;
-        private ListBox flightInfoLstBox;
         private ListBox infoTicketLstBox;
         private ComboBox ticketPickComboBox;
         private Button datVeBtn;
+        private BindingSource airportBindingSource;
+        private Button Find_Btn;
     }
 }
--- Service/DatVeForm.cs
+++ Service/DatVeForm.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using DataAccess.DAO;
+using DataAccess.DTO;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -15,6 +18,148 @@ namespace Service
         public DatVeForm()
         {
             InitializeComponent();
+        }
+
+        private string MaSanBayDi = "", MaSanBayDen = "";
+
+        private string MaChuyenBay = "";
+
+        private void flightListLstBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (flightListLstBox.SelectedItem != null)
+            {
+                MaChuyenBay = flightListLstBox.SelectedItem.ToString();
+                MaChuyenBay = MaChuyenBay.Substring(0, MaChuyenBay.IndexOf('|') - 1);
+            }
+
+            string query = "SELECT * FROM [dbo].CHUYENBAY WHERE MaChuyenBay = @MaChuyenBay ";
+            DataTable dt = DataProvider.Instance.ExecuteQuery(query, new object[] { MaChuyenBay });
+        }
+
+        private void modify_masanbay()
+        {
+            if (fromComboBox.SelectedItem != null)
+            {
+                MaSanBayDi = fromComboBox.SelectedItem.ToString();
+                MaSanBayDi = MaSanBayDi.Substring(0, MaSanBayDi.IndexOf('|') - 1);
+            }
+
+            if (toComboBox.SelectedItem != null)
+            {
+                MaSanBayDen = toComboBox.SelectedItem.ToString();
+                MaSanBayDen = MaSanBayDen.Substring(0, MaSanBayDen.IndexOf('|') - 1);
+            }
+        }
+
+        private void fromComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            modify_masanbay();
+
+            string query = "SELECT * FROM [dbo].SANBAY WHERE MaSanBay != @MaSanBayDi AND MaSanBay != @MaSanBayDen  ";
+            DataTable dt = DataProvider.Instance.ExecuteQuery(query, new object[] { MaSanBayDi, MaSanBayDen });
+
+            toComboBox.BeginUpdate();
+            toComboBox.Items.Clear();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                toComboBox.Items.Add(dr["MaSanBay"].ToString().TrimEnd() + " | " + dr["TenSanBay"]);
+            }
+
+            toComboBox.EndUpdate();
+        }
+
+        private void toComboBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            modify_masanbay();
+
+            string query = "SELECT * FROM [dbo].SANBAY WHERE MaSanBay != @MaSanBayDi AND MaSanBay != @MaSanBayDen  ";
+            DataTable dt = DataProvider.Instance.ExecuteQuery(query, new object[] { MaSanBayDi, MaSanBayDen });
+
+            fromComboBox.BeginUpdate();
+            fromComboBox.Items.Clear();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                fromComboBox.Items.Add(dr["MaSanBay"].ToString().TrimEnd() + " | " + dr["TenSanBay"]);
+            }
+
+            fromComboBox.EndUpdate();
+        }
+
+        private void DatVeForm_Load(object sender, EventArgs e)
+        {
+            string query = "SELECT * FROM [dbo].SANBAY ";
+            DataTable dt = DataProvider.Instance.ExecuteQuery(query);
+
+            fromComboBox.BeginUpdate();
+            toComboBox.BeginUpdate();
+            fromComboBox.Items.Clear();
+            toComboBox.Items.Clear();
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                toComboBox.Items.Add(dr["MaSanBay"].ToString().TrimEnd() + " | " + dr["TenSanBay"]);
+                fromComboBox.Items.Add(dr["MaSanBay"].ToString().TrimEnd() + " | " + dr["TenSanBay"]);
+            }
+
+            fromComboBox.EndUpdate();
+            toComboBox.EndUpdate();
+        }
+
+        private void list_flight()
+        {
+            modify_masanbay();
+
+            flightListLstBox.BeginUpdate();
+            flightListLstBox.Items.Clear();
+            
+            if (ngayBayDtp.Value == null)
+            {
+                flightListLstBox.EndUpdate();
+                return;
+            }
+
+            string ngaybay = DateTime.Parse(ngayBayDtp.Value.ToString()).ToString("yyyy-MM-dd");
+
+            string query = "SELECT * FROM [dbo].THAMSO ";
+            DataTable dt = DataProvider.Instance.ExecuteQuery(query);
+
+            int tg_dat_ve_cham_nhat = int.MaxValue;
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                tg_dat_ve_cham_nhat = Convert.ToInt32(dr["TGDatVeChamNhat"]);
+            }
+
+            DateTime ngaybaymin = Convert.ToDateTime(ngaybay + " 00:00:00.000");
+            DateTime ngaybaymax = Convert.ToDateTime(ngaybay + " 23:59:59.999");
+
+            query = "SELECT * FROM [dbo].CHUYENBAY WHERE NgayGioBay >= @ngaybaymin AND NgayGioBay <= @ngaybaymax AND MaSanBayDi = @MaSanBayDi AND MaSanBayDen = @MaSanBayDen ";
+            dt = DataProvider.Instance.ExecuteQuery(query, new object[] { ngaybaymin , ngaybaymax , MaSanBayDi, MaSanBayDen });
+
+            foreach (DataRow dr in dt.Rows)
+            {
+                DateTime NgayGioBay = Convert.ToDateTime(dr["NgayGioBay"]);
+                TimeSpan chk = NgayGioBay.Subtract(DateTime.Now);
+                if (chk.Days >= tg_dat_ve_cham_nhat) flightListLstBox.Items.Add(dr["MaChuyenBay"].ToString().TrimEnd() + " | " + dr["MaSanBayDi"].ToString().TrimEnd() + " | " + dr["MaSanBayDen"].ToString().TrimEnd() + " | " + dr["NgayGioBay"].ToString());
+            }
+
+            flightListLstBox.EndUpdate();
+        }
+
+        private void ngayBayDtp_ValueChanged(object sender, EventArgs e)
+        {
+            modify_masanbay();
+        }
+
+        private void flightInfoLstBox_SelectedIndexChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void Find_Btn_Click(object sender, EventArgs e)
+        {
+            list_flight();
         }
     }
 }
