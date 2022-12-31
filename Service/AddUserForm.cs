92ec0968a4d3f103c9fca07a022cbfff568f565f
hhoangcpascal <havanhoang2911@gmail.com>
2022-12-31 21:49:04 +0700

    fix

--- Service/AddUserForm.cs
+++ Service/AddUserForm.cs
@@ -31,6 +31,7 @@ namespace Service
 
         private void Add_btn_Click(object sender, EventArgs e)
         {
+            int i;
             string MaDangNhap = this.username_txtBox.Text;
             if (MaDangNhap.Length > 0)
             {
@@ -40,7 +41,7 @@ namespace Service
                     return;
                 }
 
-                for (int i = 0; i < MaDangNhap.Length; ++i)
+                for (i = 0; i < MaDangNhap.Length; ++i)
                     if (!IsDigit(MaDangNhap[i]) && !IsLowerChar(MaDangNhap[i]))
                     {
                         MessageBox.Show("Tên đăng nhập không đúng định dạng!");
@@ -83,7 +84,7 @@ namespace Service
             }
 
             query = "INSERT INTO [dbo].NGUOIDUNG VALUES ( @MaDangNhap , @MatKhau , @MaNhom , @TenNguoiDung , NULL , @SoDienThoai , NULL ,  @NgaySinh )";
-            int i = DataProvider.Instance.ExecuteNonQuery(query, new object[] { MaDangNhap, "1A8462AD95FC28007C67106DC6667AC2637FC1E9CAE30025EF5B46C5F6E9F9312FD48661626D85610A35844E5FC658DD1F5298630138D9F67EFE3074921C41B0", MaNhom, "000000000000", "0000000000", "2001-01-01" });
+            i = DataProvider.Instance.ExecuteNonQuery(query, new object[] { MaDangNhap, "1A8462AD95FC28007C67106DC6667AC2637FC1E9CAE30025EF5B46C5F6E9F9312FD48661626D85610A35844E5FC658DD1F5298630138D9F67EFE3074921C41B0", MaNhom, "000000000000", "0000000000", "2001-01-01" });
 
             MessageBox.Show("Đã thêm user mới!");
         }
